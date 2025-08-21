6e14e6e585145b3fe31eb92458fe2cc28c6dafee
Kazure Zheng <kazure@debian>
2025-08-21 10:56:44 +0800

    finish hw10

--- HW/hw01/hw01.py
+++ HW/hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i*i + j*j + k*k - max(i,max(j, k))*max(i, max(j, k))
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -66,6 +66,11 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    ans = 0
+    for i in range(1, n):
+        if (n%i == 0):
+            ans = max(ans, i)
+    return ans    
 
 
 def hailstone(n):
@@ -89,3 +94,13 @@ def hailstone(n):
     """
     "*** YOUR CODE HERE ***"
 
+    print(n)
+    count = 1
+    while(n!=1):
+        if(n%2 == 0):
+            n//=2
+        else:
+            n = n*3+1
+        print(n)
+        count+=1
+    return count
