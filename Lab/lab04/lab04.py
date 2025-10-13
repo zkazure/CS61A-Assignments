37d098ef4aaa96b1e20151ed9815e25269bddd5e
Kazure Zheng <kazure@debian>
2025-10-13 10:27:51 +0800

    finish lab04

--- Lab/lab04/lab04.py
+++ Lab/lab04/lab04.py
@@ -1,5 +1,5 @@
 def divide(quotients, divisors):
-    """Return a dictonary in which each quotient q is a key for the list of
+    """Return a dictionary in which each quotient q is a key for the list of
     divisors that it divides evenly.
 
     >>> divide([3, 4, 5], [8, 9, 10, 11, 12])
@@ -7,7 +7,7 @@ def divide(quotients, divisors):
     >>> divide(range(1, 5), range(20, 25))
     {1: [20, 21, 22, 23, 24], 2: [20, 22, 24], 3: [21, 24], 4: [20, 24]}
     """
-    return {____: ____ for ____ in ____}
+    return {q : [d for d in divisors if d%q==0] for q in quotients}
 
 
 def buy(fruits_to_buy, prices, total_amount):
@@ -29,10 +29,10 @@ def buy(fruits_to_buy, prices, total_amount):
             print(cart)
         elif fruits and amount > 0:
             fruit = fruits[0]
-            price = ____
-            for k in ____:
+            price = prices[fruit]
+            for k in range(1, amount // price+1):
                 # Hint: The display function will help you add fruit to the cart.
-                add(____, ____, ____)
+                add(fruits[1:], amount-k*price, cart + display(fruit, k))
     add(fruits_to_buy, total_amount, '')
 
 
@@ -67,6 +67,10 @@ def distance(city_a, city_b):
     5.0
     """
     "*** YOUR CODE HERE ***"
+    x = (get_lat(city_a) - get_lat(city_b)) ** 2;
+    y = (get_lon(city_a) - get_lon(city_b)) ** 2;
+    return sqrt(x+y);
+    
 
 def closer_city(lat, lon, city_a, city_b):
     """
@@ -84,6 +88,13 @@ def closer_city(lat, lon, city_a, city_b):
     'Bucharest'
     """
     "*** YOUR CODE HERE ***"
+    city_tmp = make_city('tmp', lat, lon)
+    dis1 = distance(city_tmp, city_a)
+    dis2 = distance(city_tmp, city_b)
+    if (dis1 < dis2):
+        return get_name(city_a)
+    return get_name(city_b)
+
 
 def check_city_abstraction():
     """
