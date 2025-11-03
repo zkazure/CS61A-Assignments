0473222eb5aee9111d24842963547b33680cddd2
Kazure Zheng <kazure@debian>
2025-11-03 21:58:51 +0800

    finish problem 11

--- Project/ants/ants.py
+++ Project/ants/ants.py
@@ -408,6 +408,11 @@ class Water(Place):
 
 # BEGIN Problem 11
 # The ScubaThrower class
+class ScubaThrower(ThrowerAnt):
+    name = 'Scuba'
+    food_cost = 6
+    is_waterproof = True
+    implemented = True
 # END Problem 11
 
 
