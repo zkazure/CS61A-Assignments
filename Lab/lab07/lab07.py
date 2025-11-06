b7620e79f955b66fd6628b6d912e6cd238a0dd92
Kazure Zheng <kazure@debian>
2025-11-06 21:27:52 +0800

    finish class practice

--- Lab/lab07/lab07.py
+++ Lab/lab07/lab07.py
@@ -40,6 +40,12 @@ class Account:
         """Return the number of years until balance would grow to amount."""
         assert self.balance > 0 and amount > 0 and self.interest > 0
         "*** YOUR CODE HERE ***"
+        time = 0
+        money = self.balance
+        while money < amount:
+            money *= 1+self.interest
+            time += 1
+        return time
 
 
 class FreeChecking(Account):
