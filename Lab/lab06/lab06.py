aedf4decdc65faa01981e2896557615e675fce16
Kazure Zheng <kazure@debian>
2025-11-06 19:54:42 +0800

    finish quesiton 2

--- Lab/lab06/lab06.py
+++ Lab/lab06/lab06.py
@@ -122,18 +122,18 @@ class Server:
     def __init__(self):
         self.clients = {}
 
-    def send(self, email):
+    def send(self, email:'Email'):
         """Append the email to the inbox of the client it is addressed to.
             email is an instance of the Email class.
         """
-        ____.inbox.append(email)
+        self.clients[email.recipient_name].inbox.append(email)
 
     def register_client(self, client):
         """Add a client to the clients mapping (which is a 
         dictionary from client names to client instances).
             client is an instance of the Client class.
         """
-        ____[____] = ____
+        self.clients[client.name] = client
 
 class Client:
     """A client has a server, a name (str), and an inbox (list).
@@ -152,15 +152,15 @@ class Client:
     >>> b.inbox[1].sender.name
     'Alice'
     """
-    def __init__(self, server, name):
+    def __init__(self, server:'Server', name):
         self.inbox = []
         self.server = server
         self.name = name
-        server.register_client(____)
+        server.register_client(self)
 
     def compose(self, message, recipient_name):
         """Send an email with the given message to the recipient."""
-        email = Email(message, ____, ____)
+        email = Email(message, self, recipient_name)
         self.server.send(email)
 
 
