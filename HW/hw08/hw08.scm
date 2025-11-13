8a0032a009f5337ef575d860b3f1833070bcd4d0
Kazure Zheng <kazure@debian>
2025-11-13 20:55:09 +0800

    finish question 2

--- HW/hw08/hw08.scm
+++ HW/hw08/hw08.scm
@@ -7,7 +7,12 @@
            (ascending? (cdr s))))
   )
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+  (cond ( (null? s) '())
+        
+        ( (pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+        ( else (my-filter pred (cdr s))))
+  )
 
 (define (interleave lst1 lst2) 'YOUR-CODE-HERE)
 
