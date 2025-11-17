9a3f8524b37c2611e67a88329e58a382160d7039
Kazure Zheng <kazure@debian>
2025-11-17 10:11:57 +0800

    finish question 3

--- Lab/lab11/lab11.scm
+++ Lab/lab11/lab11.scm
@@ -1,9 +1,16 @@
+#lang racket
+
 (define (if-program condition if-true if-false)
   `(if ,condition ,if-true ,if-false))
 
 (define (square n) (* n n))
 
-(define (pow-expr base exp) 'YOUR-CODE-HERE)
+(define (pow-expr base exp)
+  (cond ( (= 0 exp) 1)
+        ( (even? exp) `(square ,(pow-expr base (/ exp 2))))
+        ( else `(* ,base ,(pow-expr base (- exp 1)))))
+  )
+
 
 (define-macro (repeat n expr)
   `(repeated-call ,n ___))
